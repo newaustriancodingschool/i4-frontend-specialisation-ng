1569dbeaab33bcfdd33db88ddc7f728d6b0419f4
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2019-05-08 06:43:04 +0200

    added prettier and participants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { pipeBind1 } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'i4-frontend-specialisation-ng';
+  participants = [
+    {
+      name: 'Zidan, Adib',
+      github: 'AdibZidan',
+      project: 'i4-angular-projects'
+    },
+    {
+      name: 'Kamalova, Aizhana',
+      github: 'janamava',
+      project: 'frontend-angular'
+    },
+    { name: 'Fernandes, Tina', github: 'tina-fernan', project: 'Angular' },
+    { name: 'Hajyahya, Marj', github: 'Marj-11', project: 'Chess-board' },
+    {
+      name: 'Hammoud, Mohamed',
+      github: 'mu7m3d',
+      project: 'Angular-frontend'
+    },
+    { name: 'Raslan, Badi', github: 'badisalim', project: 'Angular' },
+    {
+      name: 'Abdo, Hamada',
+      github: 'hamadeabdo',
+      project: 'myFirstAngularApp'
+    },
+    { name: 'Khamam, Hala', github: 'Halakh', project: 'Angular' }
+  ].sort((p1, p2) => (p1.name > p2.name ? 1 : -1));
 }
